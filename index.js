781134bf7d7aeb40076659a2d88881e016ca8daf
florence <loracodes3@gmail.com>
2024-09-27 13:58:03 +0300

    litrals

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 "loracodes".charAt(1)
 console.log("loracodes".charAt(1))
 //it will output o because indexes starts at 0,1,2 and so on
+
+// the above can also be written by first declaring the name
+const name ="loracodes";
+console.log(`${name.charAt(2)}`)
+//it will output r
