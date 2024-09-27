6d69eefebe628b019594ce3c6afc61f6b0351d65
florence <loracodes3@gmail.com>
2024-09-27 14:06:28 +0300

    treat a string as an array like object to access a character

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //ways of accessing a particilar character in a string
+//1.charAt()
 "loracodes".charAt(1)
 console.log("loracodes".charAt(1))
 //it will output o because indexes starts at 0,1,2 and so on
@@ -7,3 +8,12 @@ console.log("loracodes".charAt(1))
 const name ="loracodes";
 console.log(`${name.charAt(2)}`)
 //it will output r
+
+//2.The other way is to treat the string as an array-like object
+'Regina'[2]
+console.log('Regina'[2])
+//it will output g
+
+const sister="Regina"
+console.log(`${sister[4]}`)
+//the output will be n
